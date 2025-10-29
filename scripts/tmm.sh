ebdc80fd6c7f98d5155ed36f45e6ef0209bc4feb
Jacob Mayes <jacobamayes@gmail.com>
2025-10-29 16:45:55 -0700

    slight change to tmm.sh

--- scripts/tmm.sh
+++ scripts/tmm.sh
@@ -11,13 +11,11 @@ if tmux has-session -t $MAIN 2>/dev/null; then
 
 else
     tmux new-session -d -s $MAIN -n "tops"
-    tmux new-window -t $MAIN
 
     tmux select-window -t $MAIN:1
     tmux send-keys -t 1 'btop' C-m
     tmux split-window -v
     tmux send-keys -t 2 'htop' C-m
-    tmux select-window -t $MAIN:2
 
-    tmux attach -t $MAIN:2
+    tmux attach -t $MAIN:1
 fi
