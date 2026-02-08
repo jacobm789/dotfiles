aa54db7624022c2a6c826e0ad69eeaad8320f63b
Jacob Mayes <jacobamayes@gmail.com>
2026-02-08 09:35:21 -0800

    fixed tmm.sh

--- scripts/tmm.sh
+++ scripts/tmm.sh
@@ -2,21 +2,20 @@
 
 MAIN="main"
 
-if tmux list-sessions | grep -q '(attached)'; then
-    tmux detach
+if [ -n "$TMUX" ]; then
+    echo "Currently in a tmux session!"
     exit 1
-fi
-
-if tmux has-session -t $MAIN 2>/dev/null; then
+elif tmux list-sessions | grep -q "$MAIN.*(attached)"; then
+    echo "$MAIN already attached elsewhere!"
+elif tmux has-session -t $MAIN 2>/dev/null; then
     tmux attach -t $MAIN
-
 else
     tmux new-session -d -s $MAIN -n "tops"
 
-    tmux select-window -t $MAIN:1
+    tmux select-window -t $MAIN
     tmux send-keys -t 1 'btop' C-m
     tmux split-window -h
     tmux send-keys -t 2 'htop' C-m
 
-    tmux attach -t $MAIN:1
+    tmux attach -t $MAIN
 fi
